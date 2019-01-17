6304169245fa21de1e67f9265603430388b449f4
Phillip Sturm <sturmphillip95@gmail.com>
2019-01-16 20:01:21 -0600

    JS added

--- public/javascript/saved.js
+++ public/javascript/saved.js
@@ -1,16 +1,16 @@
 getArticles = () => {
-    $.get('/api/articles').then(articles => {
+    $.get('/api/saved-articles').then(articles => {
         // console.log(articles)
-        alert('Getting articles from the database.')
+        alert('Getting saved articles from the database.')
         articles.forEach(article => {
             displayArticle(article)
         })
     }).then(() => {
-        if ($('#article-section').children().length < 1) {
+        if ($('#saved-article-section').children().length < 1) {
             alert('No articles found.')
             let emptyMessage = $('<h2>')
             emptyMessage.addClass('empty-message')
-            emptyMessage.text('There are no articles saved in the database. Click the Scrape Articles button to scrape news articles.')
+            emptyMessage.text('There are no saved articles in the database. Go back to the home page to scrape and save articles.')
             $('#article-section').append(emptyMessage)
         }
     })
@@ -49,87 +49,47 @@ displayArticle = (article) => {
     viewNoteButton.attr({
         'data-article-id': article._id,
         'data-toggle': 'modal',
-        'data-target': '#article-notes'
+        'data-target': '#saved-article-notes'
     })
     viewNoteButton.text('View Notes')
     let saveArticleButton = $('<button>')
-    saveArticleButton.addClass('btn btn-warning save-article-button')
+    saveArticleButton.addClass('btn btn-warning unsave-article-button')
     saveArticleButton.attr({
         'data-article-id': article._id,
-        'data-saved': 'unsaved'
+        'data-saved': 'saved'
     })
-    saveArticleButton.text('Save Article')
-    let deleteArticleButton = $('<button>')
-    deleteArticleButton.addClass('btn btn-danger delete-article-button')
-    deleteArticleButton.attr('data-article-id', article._id)
-    deleteArticleButton.text('Delete Article')
-    cardFooter.append(viewNoteButton, saveArticleButton, deleteArticleButton)
+    saveArticleButton.text('Unsave Article')
+    cardFooter.append(viewNoteButton, saveArticleButton)
     cardDiv.append(cardHeader, cardBody, cardFooter)
-    $('#article-section').append(cardDiv)
+    $('#saved-article-section').append(cardDiv)
 }
 
-$('#scrape-articles').on('click', event => {
-    event.preventDefault()
-    // console.log('Clicked')
-    alert('Scraping articles. Please wait.')
-    $.get('/api/scrape').then(data => {
-        $.get('/api/articles').then(articles => {
-            // console.log(articles)
-            alert('Scrape Complete')
-            $('.empty-message').remove()
-            articles.forEach(article => {
-                displayArticle(article)
-            })
-        })
-    })
-})
-
-$('#clear-articles').on('click', event => {
-    event.preventDefault()
-    console.log('Clicked')
-    $('#article-section').empty()
-    let emptyMessage = $('<h2>')
-    emptyMessage.addClass('empty-message')
-    emptyMessage.text('There are no articles saved in the database. Click the Scrape Articles button to scrape news articles.')
-    $('#article-section').append(emptyMessage)
-    alert('Articles Cleared!')
-})
-
-$(document).on('click', '.save-article-button', event => {
+$(document).on('click', '.unsave-article-button', event => {
     event.preventDefault()
     // console.log('clicked')
     var target = $(event.currentTarget)
     var id = target.data('article-id')
     // console.log(id)
-    $.post(`/api/save/${id}`).then(data => {
+    $.post(`/api/unsave/${id}`).then(data => {
         // console.log('Success!')
         // console.log(data)
         // target.text('Unsave Article')
         // target.attr('data-saved', 'saved')
         $("div").find(`[data-article-id='${id}']`).remove()
-        alert('Article Saved')
-    })
-})
-
-$(document).on('click', '.delete-article-button', event => {
-    event.preventDefault()
-    var target = $(event.currentTarget)
-    var id = target.data('article-id')
-    $.post(`/api/delete/${id}`).then(data => {
-        $("div").find(`[data-article-id='${id}']`).remove()
+        alert('Article Unsaved')
     })
 })
 
 $(document).on('click', '.view-notes-button', event => {
     event.preventDefault()
-    $('#add-note-button').remove()
+    $('#saved-add-note-button').remove()
     $('.article-notes-div').empty()
     var target = $(event.currentTarget)
     var id = target.data('article-id')
     let addNoteButton = $('<button>')
     addNoteButton.addClass('btn btn-success')
     addNoteButton.attr({
-        'id': 'add-note-button',
+        'id': 'saved-add-note-button',
         'data-article-id': id
     })
     addNoteButton.text('Add Note')
@@ -146,7 +106,7 @@ $(document).on('click', '.view-notes-button', event => {
             let noteFooter = $('<div>')
             noteFooter.addClass('card-footer')
             let deleteNoteButton = $('<button>')
-            deleteNoteButton.addClass('btn btn-danger delete-note-button')
+            deleteNoteButton.addClass('btn btn-danger saved-delete-note-button')
             deleteNoteButton.attr('data-article-id', id)
             deleteNoteButton.attr('data-note-id', data.notes[i]._id)
             deleteNoteButton.text('Delete Note')
@@ -154,29 +114,29 @@ $(document).on('click', '.view-notes-button', event => {
             noteCard.append(noteBody, noteFooter)
             $('.article-notes-div').append(noteCard)
         }
-        if ($('.article-notes-div').children().length < 1) {
+        if ($('.saved-article-notes-div').children().length < 1) {
             let emptyMessage = $('<h6>')
             emptyMessage.addClass('empty-message')
             emptyMessage.text('There are no notes saved for this Article. Use the form below to submit a Note for this Article.')
-            $('.article-notes-div').append(emptyMessage)
+            $('.saved-article-notes-div').append(emptyMessage)
         }
     })
 })
 
-$(document).on('click', '#add-note-button', event => {
+$(document).on('click', '#saved-add-note-button', event => {
     event.preventDefault()
     let target = $(event.currentTarget)
     let id = target.data('article-id')
-    let note = $('#add-note-textarea').val()
+    let note = $('#saved-add-note-textarea').val()
     // console.log(note)
     // $('#add-note-textarea').val('')
     $.post(`/api/note/${id}`, {
         text: note
     }).then(data => {
-        $('#add-note-textarea').val('')
+        $('#saved-add-note-textarea').val('')
         // console.log(data)
         $.get(`/api/notes/${id}`).then(data => {
-            $('.article-notes-div').empty()
+            $('.saved-article-notes-div').empty()
             for (let i = 0; i < data.notes.length; i++) {
                 // console.log(data.notes[i].text)
                 let noteCard = $('<div>')
@@ -187,30 +147,30 @@ $(document).on('click', '#add-note-button', event => {
                 let noteFooter = $('<div>')
                 noteFooter.addClass('card-footer')
                 let deleteNoteButton = $('<button>')
-                deleteNoteButton.addClass('btn btn-danger delete-note-button')
+                deleteNoteButton.addClass('btn btn-danger saved-delete-note-button')
                 deleteNoteButton.attr('data-article-id', id)
                 deleteNoteButton.attr('data-note-id', data.notes[i]._id)
                 deleteNoteButton.text('Delete Note')
                 noteFooter.append(deleteNoteButton)
                 noteCard.append(noteBody, noteFooter)
-                $('.article-notes-div').append(noteCard)
+                $('.saved-article-notes-div').append(noteCard)
             }
         })
     })
 })
 
-$(document).on('click', '.delete-note-button', event => {
+$(document).on('click', '.saved-delete-note-button', event => {
     let target = $(event.currentTarget)
     let articleID = target.data('article-id')
     let noteID = target.data('note-id')
     $.post(`/api/delete/${noteID}/${articleID}`).then(data => {
         if (data === 'Note Deleted') {
             target.closest('.card').remove()
-            if ($('.article-notes-div').children().length < 1) {
+            if ($('.saved-article-notes-div').children().length < 1) {
                 let emptyMessage = $('<h6>')
                 emptyMessage.addClass('empty-message')
                 emptyMessage.text('There are no notes saved for this Article. Use the form below to submit a Note for this Article.')
-                $('.article-notes-div').append(emptyMessage)
+                $('.saved-article-notes-div').append(emptyMessage)
             }
         }
     })
